c37329f928a2ac6d8f1adba38411bf5296b19746
Maria Ferraro <mariatitiferraroo@gmail.com>
2023-01-12 16:18:43 -0300

    New sections, Header, responsive and dark mode. Home page finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,29 @@
 
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        'primary': '#0EA6B1',
+      },
       backgroundImage : {
         'beach':'url("../img/fondo-playa.jpg")',
         'water':'url("../img/agua-playa.png")',
-        'expBorabora':'url("../img/experiencia-borabora.jpg")',
         'viajesSouthIsland':'url("../img/viajes-southisland.jpg")',
         'viajesBorabora':'url("../img/viajes-borabora.jpg")',
         'viajesParis':'url("../img/viajes-paris.jpg")',
         'viajesMaui':'url("../img/viajes-maui.jpg")',
         'viajesLondon':'url("../img/viajes-london.jpg")',
-        'viajesRome':'url("../img/viajes-rome.jpg")'
+        'viajesRome':'url("../img/viajes-rome.jpg")',
+        'expBorabora':'url("../img/experiencia-borabora.jpg")',
+        'expBoraboraLg':'url("../img/experiencia-borabora-lg.jpg")',
+        'expYellowknife':'url("../img/experiencia-yellowknife.jpg")',
+        'expCalafate':'url("../img/experiencia-calafate.jpg")',
+        'expFrance':'url("../img/experiencia-france.jpg")',
+        'expCapadocia':'url("../img/experiencia-capadocia.jpg")',
+        'footer':'url("../img/footer-bg.jpg")',
+        'footer-dark':'url("../img/footer-bg-darkmode.png")'
       },
       backgroundColor: theme => ({
         ...theme('colors'),
@@ -24,10 +35,10 @@ module.exports = {
       textColor:{
         'primary': '#000000',
         'secundary': '#0EA6B1',
-        'terciary': '#959595',
+        'terciary': '#959595'
       },
       fontSize:{
-        'big': ['16vw', {
+        'big': ['20vw', {
           lineHeight: '1'
         }],
       }
