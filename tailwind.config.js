2fc91a0a456b98c9bbbeeb7b47d6cb07f4cba6b6
Maria Ferraro <mariatitiferraroo@gmail.com>
2023-01-13 21:47:10 -0300

    Responsive fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./docs/index.html", "./docs/travel.html"],
+  content: ["./public/index.html", "./public/travel.html"],
   darkMode: 'class',
   theme: {
     extend: {
@@ -53,6 +53,7 @@ module.exports = {
       },
       screens: {
         '3xl': '1600px',
+        'mm': '900px'
       },
     },
     fontFamily:{
